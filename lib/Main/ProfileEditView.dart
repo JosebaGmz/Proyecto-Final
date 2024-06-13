a71ebb9cff09cbde67b3feba87ad52e1ffbc0a6e
JosebaGmz <josebagomezcaballero90@gmail.com>
2024-06-13 13:15:11 +0200

    UPDATE (nº24): Implementados los cambios necesarios para modificar foto de perfil y App Terminada.

--- lib/Main/ProfileEditView.dart
+++ lib/Main/ProfileEditView.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 import '../FirestoreObjects/FbUsuario.dart';
 
@@ -15,6 +21,9 @@ class _ProfileEditViewState extends State<ProfileEditView> {
   final TextEditingController _sizeController = TextEditingController();
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final User? _currentUser = FirebaseAuth.instance.currentUser;
+  String? _photoURL;
+  File? _imageFile;
+  Uint8List? _webImage;
 
   @override
   void initState() {
@@ -30,6 +39,7 @@ class _ProfileEditViewState extends State<ProfileEditView> {
         _nameController.text = user.nombre ?? '';
         _ageController.text = user.edad.toString();
         _sizeController.text = user.talla.toString();
+        _photoURL = userDoc.data()?['urlImg']; // Cargar la URL de la foto de perfil
       });
     }
   }
@@ -37,12 +47,53 @@ class _ProfileEditViewState extends State<ProfileEditView> {
   Future<void> _updateUserData() async {
     if (_currentUser != null) {
       String uid = _currentUser!.uid;
+      String? photoURL = _photoURL;
+
+      if (_imageFile != null || _webImage != null) {
+        photoURL = await _uploadProfileImage();
+      }
+
       await _firestore.collection('Usuarios').doc(uid).update({
         'nombre': _nameController.text,
         'edad': int.parse(_ageController.text),
         'talla': double.parse(_sizeController.text),
+        'urlImg': photoURL,
       });
+
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Perfil actualizado')));
+    }
+  }
+
+  Future<String> _uploadProfileImage() async {
+    String fileName = 'profile_${_currentUser!.uid}.png';
+    Reference storageRef = FirebaseStorage.instance.ref().child('profile_images').child(fileName);
+
+    if (kIsWeb) {
+      UploadTask uploadTask = storageRef.putData(_webImage!);
+      TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+      return await taskSnapshot.ref.getDownloadURL();
+    } else {
+      UploadTask uploadTask = storageRef.putFile(_imageFile!);
+      TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+      return await taskSnapshot.ref.getDownloadURL();
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      if (kIsWeb) {
+        final bytes = await pickedFile.readAsBytes();
+        setState(() {
+          _webImage = bytes;
+        });
+      } else {
+        setState(() {
+          _imageFile = File(pickedFile.path);
+        });
+      }
     }
   }
 
@@ -58,6 +109,21 @@ class _ProfileEditViewState extends State<ProfileEditView> {
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
+            GestureDetector(
+              onTap: _pickImage,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: _imageFile != null
+                    ? FileImage(_imageFile!)
+                    : _webImage != null
+                    ? MemoryImage(_webImage!)
+                    : _photoURL != null
+                    ? NetworkImage(_photoURL!) as ImageProvider
+                    : NetworkImage("https://previews.123rf.com/images/yupiramos/yupiramos1705/yupiramos170514531/77987158-dise%C3%B1o-gr%C3%A1fico-del-ejemplo-del-vector-del-icono-del-perfil-del-hombre-joven.jpg"),
+                child: Icon(Icons.edit),
+              ),
+            ),
+            SizedBox(height: 20),
             TextField(
               controller: _nameController,
               decoration: InputDecoration(labelText: 'Nombre'),
